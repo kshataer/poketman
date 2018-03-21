8e0e1dfc5d607d76f1877081646d95d03476c720
Kasimu SHATAER <kasimu.shataer@production.infra>
2018-03-21 17:22:27 +0100

    poket man v4

--- src/AppBundle/Form/PoketmanType.php
+++ src/AppBundle/Form/PoketmanType.php
@@ -5,7 +5,8 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 class PoketmanType extends AbstractType
 {
     /**
@@ -14,10 +15,10 @@ class PoketmanType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('name')
-        ->add('age')
-        ->add('type')
-        ->add('color');
+        ->add('name',TextType::class)
+        ->add('age', IntegerType::class)
+        ->add('type', TextType::class)
+        ->add('color', TextType::class);
     }/**
      * {@inheritdoc}
      */
