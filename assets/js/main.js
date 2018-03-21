620350d7ccdebcfd54c698be9e5e2031be0f4947
Kasimu SHATAER <kasimu.shataer@production.infra>
2018-03-21 16:19:22 +0100

    poketman v4

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 const $ = require('jquery');
 const Routing = require('./modules/Routing');
 require('materialize-css');
-require('material-design-lite');
 require('./modules/alert')($);
 
 global.$ = global.jQuery = $;
